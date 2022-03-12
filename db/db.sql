121ec1a463ff1e5d911625c9e4b960d994a8e2a6
unknown <christiandc19@hotmail.com>
2022-03-12 00:54:42 -0800

    created an employee database

---
+++ db/db.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS employee_information;
+CREATE DATABASE employee_information;
+USE employee_information;
---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO departments (name)
+VALUES
+    ('IT Department'),
+    ('HR Department');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('IT Manager', 75000, 1),
+    ('IT Staff', 50000, 1),
+    ('HR Manager', 45000, 2),
+    ('HR Staff', 35000, 2);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+    ('Joey', 'Tribiani', 1),
+    ('Rachel', 'Green', 2),
+    ('Monica', 'Geller', 2),
+    ('Phoebe', 'Buffay', 3),
+    ('Chandler', 'Bing', 4),
+    ('Ross', 'Geler', 4);
+
+UPDATE employees SET manager_id = 1 WHERE role_id = 2 
+UPDATE employees SET manager_id = 4 WHERE role_id = 4;
